4865d3ce1623c6d26b8bd499e37be33b50f22104
DilanMDA <akiladilan04@gmail.com>
2022-04-10 13:19:09 +0530

    header created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Registration from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Header from "./components/nav/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Registration />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Registration />} />
+      </Routes>
+    </>
   );
 }
 
