f2cc7838bab096d43d1eb933feae7211de952cf6
DilanMDA <akiladilan04@gmail.com>
2022-04-10 12:58:14 +0530

    page routing

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Routes>
-      <Route exact path='/' element={<Home />} />
-      <Route exact path='/login' element={<Login />} />
-      <Route exact path='/register' element={<Registration />} />
+      <Route index element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Registration />} />
     </Routes>
   );
 }
